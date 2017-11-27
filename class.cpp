b0207e116b72ed130c876ed6539ab5199c4841c4
jacqueshero <33997397+jacqueshero@users.noreply.github.com>
2017-11-28 00:50:56 +0800

    class.cpp
    help im so lost

---
+++ class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class console {
+helloworld = "hello world.\n";
+hi = "hey cherie, cuntface./n";
+noyou = "look at your phone.\n";
+
+function output(msg) {
+msg = arg;
+cout << arg << endl;
+ }
+}
